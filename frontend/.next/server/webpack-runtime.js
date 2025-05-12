8d7b988969913eea07f558b104a51519de0476ba
Zeus1411 <121225888+zeus1411@users.noreply.github.com>
2025-05-12 23:45:14 +0700

    Các file tự sinh

--- frontend/.next/server/webpack-runtime.js
+++ frontend/.next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("ca721c143ac66493")
+/******/ 		__webpack_require__.h = () => ("ad2f371f22a264b2")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
